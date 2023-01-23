6b6e382c1c35dad3a7af358389cdd36e8c829d76
kostekd <dominikkostencki99@gmail.com>
2023-01-23 20:54:47 +0100

    Prepare structure for endpoints and download react-quert

---
+++ client/src/services/service.ts
@@ -0,0 +1,16 @@
+
+
+export namespace Service {
+  export interface QueryParams {
+        name: string;
+        minAge: number;
+        maxAge: number;
+    }
+    export interface PostBody {
+        name: string;
+        age: number;
+    }
+
+
+
+}
